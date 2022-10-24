a1e84da9756aaef376be81a1870ab8ec62ade8c7
arkadeepg <112115235+arkadeepg@users.noreply.github.com>
2022-10-24 15:12:07 +0530

    Program to find the highest number out of 3 numbers.

---
+++ Highest Number.py
@@ -0,0 +1,26 @@
+# Highest Number among three
+
+first = int(input("Enter First number : "))
+second = int(input("Enter Second Number : "))
+third = int(input("Enter Third Number : "))
+
+# if (first > second) & (first > third):
+#     highest = first
+# elif (second > first) & (second > third):
+#     highest = second
+# else:
+#     highest = third
+
+# highest = 00
+# int(highest)
+highest = first
+if second > highest:
+    if second > third:
+        highest = second
+    else:
+        highest = third
+elif third > highest:
+    if third > second:
+        highest = third
+
+print("Highest number is " + str(highest) + ".")
