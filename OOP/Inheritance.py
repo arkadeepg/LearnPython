66c45e720ebd94edfb6ff529c4780afbf636056b
arkadeepg <112115235+arkadeepg@users.noreply.github.com>
2022-10-26 13:53:45 +0530

    OOP concept

--- OOP/Inheritance.py
+++ OOP/Inheritance.py
@@ -1,2 +1,37 @@
 # Program to understand Inheritance
 
+# Single Inheritance
+class base:
+    def display(self):
+        print("Parent")
+
+
+class derived(base):
+    def show(self):
+        print("Child")
+
+
+obj1 = derived()
+
+obj1.display()
+
+
+# Multi-level Inheritance
+
+class grandparent:
+    def gdisplay(self):
+        print("Grandparent Class")
+
+class parent(grandparent):
+    def pdisplay(self):
+        print("Parent Class")
+
+class child(parent):
+    def cdisplay(self):
+        print("Child Class")
+
+obj2 = child()
+
+obj2.gdisplay()
+obj2.pdisplay()
+obj2.cdisplay()
